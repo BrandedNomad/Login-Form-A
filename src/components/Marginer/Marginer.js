141baf8507a267c43f091b8ab83e656d63aed009
BrandedNomad <info@brandednomad.com>
2021-04-03 19:38:03 +0700

    feat: add Marginer to loginForm

---
+++ src/components/Marginer/Marginer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Marginer(props){
+    return (
+        <div style={{height:props.height,width:100}}>
+
+        </div>
+    )
+}
+
+export default Marginer;
--- src/components/loginBox/loginForm.js
+++ src/components/loginBox/loginForm.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {BoxContainer, FormContainer, Input, MutedLink, SubmitButton} from "./common";
-
+import Marginer from "../Marginer/Marginer";
 function LoginForm(props){
     return (
         <BoxContainer>
             <FormContainer>
                 <Input type='email' placeholder='Email'/>
                 <Input type='email' placeholder='Password'/>
+                <Marginer height={20}/>
                 <MutedLink href='#'>Forget your password?</MutedLink>
+                <Marginer height={20}/>
                 <SubmitButton type='submit'>Signin</SubmitButton>
             </FormContainer>
         </BoxContainer>
